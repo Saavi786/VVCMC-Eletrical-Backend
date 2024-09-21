e9d851d4121edc22864bebfa911bef9a405e9abf
HarshitRaul24 <harshitraul15@gmail.com>
2024-09-21 18:15:14 +0530

    changes

--- config/DBConn.js
+++ config/DBConn.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 
-const DB = "mongodb://127.0.0.1:27017/testvv?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.3.1";
+const DB = "mongodb://127.0.0.1:27017/VVCM?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.3.1";
 
 mongoose.connect(DB).then(()=>console.log("DB Connected")).catch((err)=>console.log(err.message));
